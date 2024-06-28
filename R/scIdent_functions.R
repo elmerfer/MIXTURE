b93fc1347ff7c7efe49f88a628a253ce1774c292
Agustin N <agx.nava@gmail.com>
2024-06-28 11:20:38 -0300

    Adding scIdent functions

---
+++ R/scIdent_functions.R
@@ -0,0 +1,340 @@
+scIdent <- function(SeuObj, clusters_metadata = NULL, pseudobulk = 1, pct = 1, ms_treshold = 0.09, sgmtx = LM22, cores = 10L, SeuratAssay = "RNA") {
+  if (!require("tidyverse", character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+    library(package, character.only = TRUE)
+  }
+  
+  if (!require("rlist", character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+    library(package, character.only = TRUE)
+  }
+  
+  if (!require("reshape2", character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+    library(package, character.only = TRUE)
+  }
+  
+  options(dplyr.summarise.inform = FALSE)
+  
+  if (pseudobulk > 1 & pct == 1) {
+    stop("If 'pseudobulk' > 1, 'pct' must range between 0.1 and 0.9")
+  }
+  
+  if (Sys.info()['sysname'] == "Windows" & cores != 1) {
+    stop("If using Windows, 'cores' must be set to 1.")
+  }
+  
+  if (is.null(clusters_metadata)) {
+    clusters <- SeuObj$seurat_clusters
+  } else {
+    clusters <- SeuObj@meta.data[,names(SeuObj@meta.data) == clusters_metadata]
+  }
+  
+  if (sum(names(SeuObj@meta.data) == clusters_metadata) == 0) {
+    stop(paste0("The column '",clusters_metadata,"' is not present in the meta.data of the Seurat object."))
+  }
+  
+  data <- SeuObj[[SeuratAssay]]$counts
+  
+  if (nrow(SeuObj@meta.data) != length(clusters)) {
+    stop("'clusters' does not equal the number of cells in the dataset.")
+  }
+  clusters <- as.character(clusters)
+  clusters[is.na(clusters)] <- "NA"
+  n <- nrow(data)
+  cluster_names <- unique(clusters)
+  results <- list()
+  cat(paste0("Generating ",pseudobulk," pseudobulk sample(s) * ", length(cluster_names)," clusters..."))
+  for (cluster in cluster_names) {
+    cluster_indices <- which(clusters == cluster)
+    cluster_size <- length(cluster_indices)
+    if (cluster_size == 1) {
+      stop("A cluster is composed by only 1 cell, pseudobulk can't be performed.")}
+    cluster_pseudobulk <- as.data.frame(matrix(0, nrow = n, ncol = pseudobulk))
+    rownames(cluster_pseudobulk) <- rownames(data)
+    names(cluster_pseudobulk) <- paste0("*sim",1:pseudobulk)
+    
+    for (i in 1:pseudobulk) {
+      selected <- sample(cluster_indices,round(cluster_size * pct))
+      bulk_subjects <- rowSums(data[,unique(selected)])
+      cluster_pseudobulk[,i] <- bulk_subjects
+    }
+    results[[paste0("Cluster_", cluster)]] <- as.data.frame(cluster_pseudobulk)
+  }
+  
+  if (pseudobulk == 1) {
+    exp_pseudobulk <- as.data.frame(rlist::list.cbind(results))
+    names(exp_pseudobulk) <- paste0(names(results),".",names(exp_pseudobulk))
+    sigOverlap <- as.data.frame(exp_pseudobulk[rownames(exp_pseudobulk) %in% rownames(LM22),])
+    names(sigOverlap) <- cluster_names
+    sigOverlap[sigOverlap > 0] <- 1
+    sigOverlap <- as.data.frame(colSums(sigOverlap)/length(rownames(sgmtx)))
+    names(sigOverlap)[1] <- "sigOverlap"
+    sigOverlap$sigOverlap <- round(sigOverlap$sigOverlap,3)
+    sigOverlap$cluster <- rownames(sigOverlap)
+    sigOverlap <- sigOverlap[,c(2,1)]
+  } else {
+    exp_pseudobulk <- as.data.frame(rlist::list.cbind(results))
+    sigOverlap <- as.data.frame(lapply(results, FUN = function(x) {x <- as.data.frame(x[rownames(x) %in% rownames(sgmtx),]);
+    x <- x %>% 
+      mutate_if(is.numeric, ~1 * (. > 0));
+    round(mean(colSums(x))/length(rownames(sgmtx)),3)}))
+    names(sigOverlap) <- cluster_names
+    sigOverlap <- as.data.frame(t(sigOverlap))
+    names(sigOverlap)[1] <- "sigOverlap"
+    sigOverlap$cluster <- rownames(sigOverlap)
+    sigOverlap <- sigOverlap[,c(2,1)]
+  }
+  
+  deconv_rs <- MIXTURE(expressionMatrix = exp_pseudobulk,
+                       signatureMatrix = sgmtx,
+                       iter = 0L,
+                       functionMixture = nu.svm.robust.RFE,
+                       useCores = cores)
+  
+  if (pseudobulk != 1) {
+    mix_abs <- as.data.frame(deconv_rs$Subjects$MIXabs) %>% 
+      mutate(cluster = gsub("\\*.*","",rownames(.))) %>% 
+      mutate(cluster = gsub("Cluster_","",cluster)) %>% 
+      mutate(cluster = substr(cluster, 1, nchar(cluster)-1)) %>% 
+      reshape2::melt(id.vars="cluster") %>%
+      mutate(value = ifelse(is.na(value),0,value)) %>% 
+      group_by(cluster,variable) %>% 
+      dplyr::summarize(median = median(value),
+                       IQR = IQR(value)) %>% 
+      ungroup() 
+    
+    clust_idents <- mix_abs  %>% 
+      group_by(cluster) %>%
+      dplyr::mutate(median_max = max(median)) %>%
+      mutate(median = ifelse(median_max < ms_treshold,NA,median),
+             IQR = ifelse(median_max < ms_treshold,NA,IQR)) %>% 
+      dplyr::summarize(MS_cluster = ifelse(is.na(max(median)),0,1)) %>% 
+      ungroup() %>% 
+      left_join(., mix_abs  %>% 
+                  group_by(cluster) %>%
+                  dplyr::mutate(median_max = max(median)) %>%
+                  filter(median_max >= ms_treshold) %>% 
+                  filter(median != 0) %>% 
+                  filter(median >= ms_treshold) %>% 
+                  dplyr::summarize(ident_1 = variable[order(median, decreasing = TRUE)][1],
+                                   ident_2 = variable[order(median, decreasing = TRUE)][2],
+                                   ident_3 = variable[order(median, decreasing = TRUE)][3]),
+                by = "cluster")
+    
+    cluster_abs <- as.data.frame(deconv_rs$Subjects$MIXabs) %>% 
+      mutate(cluster = gsub("\\*.*","",rownames(.))) %>% 
+      mutate(cluster = gsub("Cluster_","",cluster)) %>% 
+      mutate(cluster = substr(cluster, 1, nchar(cluster)-1)) %>% 
+      reshape2::melt(id.vars="cluster") %>%
+      mutate(value = ifelse(is.na(value),0,value)) %>% 
+      group_by(cluster,variable) %>% 
+      dplyr::summarize(median = median(value)) %>% 
+      ungroup() %>%
+      pivot_wider(names_from = variable, values_from = median)
+    cluster_abs[,-1][cluster_abs[,-1] < ms_treshold] <- 0
+  } else {
+    mix_abs <- as.data.frame(deconv_rs$Subjects$MIXabs) %>% 
+      mutate(cluster = gsub("\\*.*","",rownames(.))) %>% 
+      mutate(cluster = gsub("Cluster_","",cluster)) %>% 
+      mutate(cluster = substr(cluster, 1, nchar(cluster)-1)) %>% 
+      reshape2::melt(id.vars="cluster") %>%
+      mutate(value = ifelse(is.na(value),0,value)) 
+    
+    clust_idents <- mix_abs  %>% 
+      group_by(cluster) %>%
+      dplyr::mutate(value_max = max(value)) %>%
+      mutate(value = ifelse(value_max < ms_treshold,NA,value)) %>% 
+      dplyr::summarize(MS_cluster = ifelse(is.na(max(value)),0,1)) %>% 
+      ungroup() %>% 
+      left_join(., mix_abs  %>% 
+                  group_by(cluster) %>%
+                  dplyr::mutate(value_max = max(value)) %>%
+                  filter(value_max >= ms_treshold) %>% 
+                  filter(value != 0) %>% 
+                  filter(value >= ms_treshold) %>% 
+                  dplyr::summarize(ident_1 = variable[order(value, decreasing = TRUE)][1],
+                                   ident_2 = variable[order(value, decreasing = TRUE)][2],
+                                   ident_3 = variable[order(value, decreasing = TRUE)][3]),
+                by = "cluster")
+    
+    cluster_abs <- as.data.frame(deconv_rs$Subjects$MIXabs) %>% 
+      mutate(cluster = gsub("\\*.*","",rownames(.))) %>% 
+      mutate(cluster = gsub("Cluster_","",cluster)) %>% 
+      mutate(cluster = substr(cluster, 1, nchar(cluster)-1)) %>% 
+      reshape2::melt(id.vars="cluster") %>%
+      mutate(value = ifelse(is.na(value),0,value)) %>% 
+      pivot_wider(names_from = variable, values_from = value) 
+    cluster_abs[,-1][cluster_abs[,-1] < ms_treshold] <- 0
+  }
+  
+  cat(paste0("\nComplete! ",sum(clust_idents$MS_cluster)," clusters contain MS cell types."))
+  
+  statAbs <- if (pseudobulk != 1) {as.data.frame(deconv_rs$Subjects$MIXabs) %>% 
+      mutate(cluster = gsub("\\*.*","",rownames(.))) %>% 
+      mutate(cluster = gsub("Cluster_","",cluster)) %>% 
+      mutate(cluster = substr(cluster, 1, nchar(cluster)-1)) %>% 
+      reshape2::melt(id.vars="cluster") %>%
+      mutate(value = ifelse(is.na(value),0,value)) %>% 
+      group_by(cluster,variable) %>% 
+      dplyr::summarize(median = median(value),
+                       IQR = IQR(value),
+                       q1 = quantile(value,0.25),
+                       q3 = quantile(value,0.75),
+                       min = min(value),
+                       max = max(value)) %>% 
+      filter(median != 0) %>% 
+      arrange(cluster,dplyr::desc(median)) %>% 
+      filter(median >= ms_treshold) %>% 
+      rename("celltype" = "variable")
+    
+  } else {NULL}
+  normalize_nonzero <- function(x) {
+    nonzero_indices <- x != 0
+    sum_nonzero <- sum(x[nonzero_indices])
+    if (sum_nonzero == 0) {
+      return(x)
+    }
+    x[nonzero_indices] <- x[nonzero_indices] / sum_nonzero
+    return(x)
+  }
+  
+  apply_normalize_nonzero <- function(df) {
+    as.data.frame(t(apply(df, 1, normalize_nonzero)))
+  }
+  
+  cluster_props <- cluster_abs
+  cluster_props[,-1] <- apply_normalize_nonzero(cluster_props[,-1])
+  
+  return(list(clust_idents = clust_idents,
+              cluster_abs = cluster_abs,
+              cluster_props = cluster_props,
+              sigOverlap = sigOverlap,
+              pseudobulk = pseudobulk,
+              pct = pct,
+              ms_treshold = ms_treshold,
+              clusters_metadata = clusters_metadata))
+}
+
+clustHeatmap <- function(scIdentObj, coef = "abs") {
+  load_package <- function(package) {
+    if (!require(package, character.only = TRUE)) {
+      install.packages(package, dependencies = TRUE)
+      library(package, character.only = TRUE)
+    }
+  }
+  
+  load_package("hrbrthemes")
+  load_package("ggplot2")
+  load_package("viridis")
+  load_package("reshape2")
+  load_package("tidyverse")
+  
+  if (!(coef %in% c("abs", "prop"))) {
+    stop("Invalid 'coef'. Choose either 'abs' or 'prop'.")
+  }
+  
+  data <- if (coef == "abs") {
+    scIdentObj$cluster_abs
+  } else {
+    scIdentObj$cluster_props
+  }
+  
+  name <- if (coef == "abs") "Absolute\ncoefficients" else "Normalized\ncoefficients"
+  
+  data <- data %>%
+    reshape2::melt(id.vars = "cluster") %>%
+    filter(value > 0) 
+  
+  data <-  rbind(data, data.frame(cluster = scIdentObj$clust_idents$cluster[which(scIdentObj$clust_idents$MS_cluster == 0)],
+                                  variable = rep(data$variable[1],length(scIdentObj$clust_idents$cluster[which(scIdentObj$clust_idents$MS_cluster == 0)])),
+                                  value = rep(NA,length(scIdentObj$clust_idents$cluster[which(scIdentObj$clust_idents$MS_cluster == 0)]))
+  ))
+  
+  data <- data %>%
+    mutate(value = if (coef == "abs") round(value, 2) else round(value, 3)) %>%
+    mutate(variable = as.character(variable)) %>%
+    complete(cluster, variable)
+  
+  p <- ggplot(data, aes(variable, cluster, fill = value)) + 
+    geom_tile(color = "black") + 
+    scale_fill_gradient2(mid = "black", high = "red", na.value = "grey", name = name) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 0.95, vjust = 0.2, size = 10, color = "black"),
+          axis.text.y = element_text(size = 10, color = "black"),
+          axis.title.x = element_text(face = "bold", color = "black"),    
+          axis.title.y = element_text(face = "bold", color = "black")) +
+    geom_label(aes(label = if (coef == "abs") value else gsub("NA%", NA, paste0(value * 100, "%"))), color = "black", fill = "grey") +
+    coord_fixed() + xlab("MS cell types") + ylab("Cluster")
+  
+  suppressWarnings(print(p))
+}
+
+PlotDimCoef <- function(SeuratObj, scIdentObj, ms_celltypes = NULL, reduction, coef = "abs", ncol = NULL, nrow = NULL) {
+  load_package <- function(package) {
+    if (!require(package, character.only = TRUE)) {
+      install.packages(package, dependencies = TRUE)
+      library(package, character.only = TRUE)
+    }
+  }
+  
+  load_package("dplyr")
+  load_package("ggplot2")
+  load_package("patchwork")
+  load_package("reshape2")
+  
+  if (!(coef %in% c("abs", "prop"))) {
+    stop("Invalid 'coef'. Choose either 'abs' or 'prop'.")
+  }
+  
+  clust_label = scIdentObj$clusters_metadata
+  cellnames <- rownames(SeuratObj@meta.data)
+  
+  cluster_data <- if (coef == "abs") {
+    scIdentObj$cluster_abs[, names(scIdentObj$cluster_abs) %in% c("cluster", ms_celltypes)]
+  } else {
+    scIdentObj$cluster_props[, names(scIdentObj$cluster_props) %in% c("cluster", ms_celltypes)]
+  }
+  
+  ms_celltypes <- if (is.null(ms_celltypes)) {unique((reshape2::melt(scIdentObj$cluster_abs,id.vars="cluster") %>% 
+                                                        filter(value > 0) %>% 
+                                                        mutate(variable = as.character(variable)))$variable)
+  } else {ms_celltypes}
+  
+  ms_coef_max <- reshape2::melt(cluster_data, id.vars = "cluster")
+  ms_coef_max <- max(ms_coef_max$value)
+  
+  SeuratObj@meta.data <- left_join(SeuratObj@meta.data,
+                                   cluster_data,
+                                   by = setNames("cluster", clust_label))
+  rownames(SeuratObj@meta.data) <- cellnames
+  
+  p1 <- FeaturePlot(object = SeuratObj, reduction = reduction, features = ms_celltypes, combine = FALSE)
+  fix.sc <- scale_color_gradientn(colours = c('lightgrey', 'blue'), limits = c(0, ms_coef_max))
+  p2 <- lapply(p1, function(x) suppressMessages(x + fix.sc))
+  
+  patchwork::wrap_plots(p2, ncol = ncol, nrow = nrow)
+}
+
+PlotDimIdent <- function(SeuratObj, scIdentObj, ident_col = "ident_1", reduction) {
+  load_package <- function(package) {
+    if (!require(package, character.only = TRUE)) {
+      install.packages(package, dependencies = TRUE)
+      library(package, character.only = TRUE)
+    }
+  }
+  
+  load_package("dplyr")
+  load_package("ggplot2")
+  
+  clust_label = scIdentObj$clusters_metadata
+  cellnames <- rownames(SeuratObj@meta.data)
+  
+  cluster_data <- scIdentObj$clust_idents[, names(scIdentObj$clust_idents) %in% c("cluster", ident_col)]
+  
+  SeuratObj@meta.data <- left_join(SeuratObj@meta.data,
+                                   cluster_data,
+                                   by = setNames("cluster", clust_label))
+  rownames(SeuratObj@meta.data) <- cellnames
+  
+  DimPlot(SeuratObj, reduction = reduction, group.by = ident_col)
+}
